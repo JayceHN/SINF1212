c95c022806e0c171bfdf0eceb9110e37a1d442cd
JayceHN <guillaume.calmant@student.uclouvain.be>
2016-05-17 13:52:57 +0200

    server.js & package.json updated

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-var hello = require('./hello');
-hello.sayHello();
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+   res.writeHead(200, { 'Content-Type': 'text/plain' });
+   res.write('Hello World!\n');
+   res.end();
+});
+
+server.listen(8080);
+
+console.log('Server running at http://localhost:8080');
