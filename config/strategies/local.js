619572ba842902ad7ef23a921bff22eda1fc4369
Céline Deknop <Cline Deknop>
2016-08-09 18:41:39 +0200

    Fix connexion : il fallait ajouter les usernameField et passwordField

--- config/strategies/local.js
+++ config/strategies/local.js
@@ -2,7 +2,11 @@ var passport = require('passport'),
 LocalStrategy = require('passport-local').Strategy,
 User = require('mongoose').model('User');
 module.exports = function() {
-  passport.use(new LocalStrategy(function(pseudo, password, done) {
+  passport.use(new LocalStrategy({
+    usernameField: 'pseudo', //Définir où passeport cherche ses infos dans le modèle mongoose
+    passwordField: 'password'
+  },
+function(pseudo, password, done) {
     User.findOne({pseudo: pseudo
     }, function(err, user) {
       if (err) {
